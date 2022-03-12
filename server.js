60b3098b00711e0fb12fbdd39d2f72932cc10082
A. Dorsey <ashadorsey@gmail.com>
2022-03-12 13:58:43 -0500

    added listen PORT

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const app = express();
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
 
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static 
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, jsonString) => {
+      newNotes = jsonString ? JSON.parse(jsonString) : [];
+      res.json(newNotes);
+
+
+  }); 
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+          });
+        })
