ae7bb14b1c3bc2e0878ece4cfaef1fac2682c5d8
A. Dorsey <ashadorsey@gmail.com>
2022-05-28 22:26:23 -0400

    fixed notesIndex.js

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -25,10 +25,6 @@ router.get("/notes", (req, res) => {
 //GET request for notes
 router.post("/notes", (req, res) => {
   // set id based on what the next index of the array will be
-  req.body.id = noteLog.length.toString();
-  let newNote = createNewNote(req.body, noteLog);
-  res.json(newNote);
-
   if (!validateNote(req.body)) {
     res.status(400).send("Enter both a title and content for the note.");
   } else {
